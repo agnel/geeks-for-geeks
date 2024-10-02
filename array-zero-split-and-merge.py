96f0da49aa51716420376a28617bb141d341714c
Agnel Waghela <agnel@users.noreply.github.com>
2024-10-02 15:06:36 +0530

    BCA Day 6: refactor: array-zero-split-and-merge.py
    This approach uses xor'ing all the elements of the array
    and then checking if the result is divisible by 2

--- array-zero-split-and-merge.py
+++ array-zero-split-and-merge.py
@@ -1,6 +1,6 @@
 # Check whether an Array can be made 0 by splitting and merging repeatedly
 # Ref: https://www.geeksforgeeks.org/check-whether-an-array-can-be-made-0-by-splitting-and-merging-repeatedly/
-# tags: bca, day-6, bit-manipulation, array-zero-split-and-merge
+# tags: bca, day-6, bit-manipulation, array-zero-split-and-merge, xor
 class ArrayZeroSplitAndMerge:
     def __init__(self, arr):
         self.arr = arr
@@ -10,8 +10,7 @@ class ArrayZeroSplitAndMerge:
         odds_count = 0
 
         for i in range(len(arr)):
-            if arr[i] % 2:
-                odds_count += 1
+            odds_count ^= arr[i]
         
 
         return not (odds_count % 2)
